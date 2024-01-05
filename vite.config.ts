8bc73bce3c815c1bda9f853a8ac8930b3537aa4f
张殃离 <96040653+ZhangYangLizzm@users.noreply.github.com>
2024-01-05 23:34:16 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,21 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig, loadEnv } from "vite";
 import svgr from "vite-plugin-svgr";
 import path from "path";
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const ENV = loadEnv(mode, process.cwd());
   return {
-    plugins: [
-      svgr(),
-      react(),],
+    plugins: [svgr(), react()],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "src"),
       },
     },
     server: {
-      port: 5174
+      port: 5174,
     },
-    base: ENV.VITE_APP_PUBLIC_PATH
-  }
-})
+    base: ENV.VITE_APP_PUBLIC_PATH,
+  };
+});
