1c6a228594f8b14fad947f62e991f7fd399b373e
张殃离 <96040653+ZhangYangLizzm@users.noreply.github.com>
2024-01-05 21:55:18 +0800

    chore: delete no use hook

--- src/components/TodoList/components/Form.tsx
+++ src/components/TodoList/components/Form.tsx
@@ -1,6 +1,12 @@
-import { useState, ChangeEvent, forwardRef, useImperativeHandle, Ref, useLayoutEffect } from 'react';
-import '../scss/TodoFrom.scss';
-import useScrollToElement from '@/hooks/useScrollToElement';
+import {
+  useState,
+  ChangeEvent,
+  forwardRef,
+  useImperativeHandle,
+  Ref,
+} from "react";
+import "../scss/TodoFrom.scss";
+import useScrollToElement from "@/hooks/useScrollToElement";
 
 export interface TodoFormData {
   title: string;
@@ -9,17 +15,16 @@ export interface TodoFormData {
   priority: string;
 }
 export interface TodoFormRef {
-  getFormData(): TodoFormData
-
+  getFormData(): TodoFormData;
 }
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const TodoForm = forwardRef((_, ref: Ref<TodoFormRef>) => {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
-  const [tags, setTag] = useState('');
-  const [priority, setPriority] = useState('');
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [tags, setTag] = useState("");
+  const [priority, setPriority] = useState("");
 
-  useScrollToElement('.todo-form')
+  useScrollToElement(".todo-form");
 
   useImperativeHandle(ref, () => ({
     getFormData() {
@@ -28,9 +33,9 @@ const TodoForm = forwardRef((_, ref: Ref<TodoFormRef>) => {
         content,
         tags,
         priority,
-      }
+      };
     },
-  }))
+  }));
 
   const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value);
@@ -44,37 +49,41 @@ const TodoForm = forwardRef((_, ref: Ref<TodoFormRef>) => {
     setTag(e.target.value);
   };
 
-  const handlePriorityChange = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>, priorityOption: string) => {
-    e.preventDefault()
+  const handlePriorityChange = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    priorityOption: string
+  ) => {
+    e.preventDefault();
     setPriority(priorityOption);
   };
 
   return (
-    <form className='todo-form'>
+    <form className="todo-form">
       <input
         type="text"
-        placeholder='Take dog out on walk'
+        placeholder="Take dog out on walk"
         id="title"
         value={title}
         onChange={handleTitleChange}
       />
       <textarea
         id="content"
-        placeholder='He needs vaccine shot too'
+        placeholder="He needs vaccine shot too"
         value={content}
-        onChange={handleContentChange} />
+        onChange={handleContentChange}
+      />
       <input
         type="text"
-        placeholder='Tags(支持以 、作为分隔生成多个Tag)'
+        placeholder="Tags(支持以 、作为分隔生成多个Tag)"
         id="tags"
         value={tags}
         onChange={handleTagsChange}
       />
-      <div className='priority-button-group' >
-        {['1', '2', '3', '4'].map((priorityOption) => (
+      <div className="priority-button-group">
+        {["1", "2", "3", "4"].map((priorityOption) => (
           <button
             key={priorityOption}
-            className={priority === priorityOption ? 'active' : ''}
+            className={priority === priorityOption ? "active" : ""}
             onClick={(e) => handlePriorityChange(e, priorityOption)}
           >
             {priorityOption}
@@ -83,6 +92,6 @@ const TodoForm = forwardRef((_, ref: Ref<TodoFormRef>) => {
       </div>
     </form>
   );
-})
+});
 
 export default TodoForm;
